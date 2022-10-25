3cd2f4377098c787825cd967e15c1760111841e4
Edgar Felipe <edgar_felipe13@hotmail.com>
2022-10-25 00:44:16 -0500

    estaba creando el mensaje por si el array estaba vacio

--- js/main.js
+++ js/main.js
@@ -19,7 +19,6 @@ document
 // hago una condicion que si no hay nada ponga que agregue el pedido, de lo contrario que muestre lo que había antes
 // y muestre la carta
 function manejadorSubmitMesa(e) {
- 
   e.preventDefault();
   mesa = document.getElementById("mesa").value;
 
@@ -27,15 +26,14 @@ function manejadorSubmitMesa(e) {
 
   const peliculas = JSON.parse(localStorage.getItem(mesa));
 
-
-  if (peliculas == null) {
+  if (peliculas == null || peliculas == "") {
     mesaSeleccionada.innerHTML = "<h1>Agregue el pedido </h1>";
   } else {
     mostrarPeliculas(peliculas);
   }
   mostrarCardsCarta();
 }
-async function mostrarCardsCarta() {
+function mostrarCardsCarta() {
   const contenedorOpciones = document.getElementById("opciones");
   opciones.innerHTML = `<h3>Platos de la mesa ${mesa}</h3>`;
   fetch(`../js/food.json`)
@@ -54,28 +52,30 @@ async function mostrarCardsCarta() {
         div.append(button);
         button.innerText = "agregar plato";
         button.addEventListener("click", () => {
-            //console.log(platos);
           agregarPelicula(plato);
         });
 
         contenedorOpciones.appendChild(div);
       });
     });
+}
+function agregarPelicula(plato) {
+  const titulo = plato.Title;
+  const duracion = plato.image;
+  const director = plato.recipe;
+  const linkTrailer = plato.price;
+  const pelicula = new Pelicula(titulo, duracion, director, linkTrailer);
 
-  //   const opciones = document.getElementById("opciones");
+  const peliculasEnLocalStorage = JSON.parse(localStorage.getItem(mesa));
 
-  //   opciones.innerHTML = `<h3>Platos de la mesa ${mesa}</h3>
-  //         <form id="formulario-pelicula">
-  //           <input type="text" id="titulo" placeholder="Titulo">
-  //           <input type="number" id="duracion" placeholder="Duracion">
-  //           <input type="text" id="director" placeholder="Director">
-  //           <input type="text" id="linkTrailer" placeholder="Link trailer">
-  //           <button type="submit">Agregar pelicula</button>
-  //         </form>`;
-
-  //   document
-  //     .getElementById("formulario-pelicula")
-  //     .addEventListener("submit", agregarPelicula);
+  if (peliculasEnLocalStorage == null) {
+    localStorage.setItem(mesa, JSON.stringify([pelicula]));
+    mostrarPeliculas([pelicula]);
+  } else {
+    peliculasEnLocalStorage.push(pelicula);
+    localStorage.setItem(mesa, JSON.stringify(peliculasEnLocalStorage));
+    mostrarPeliculas(peliculasEnLocalStorage);
+  }
 }
 
 function mostrarPeliculas(peliculas) {
@@ -103,47 +103,17 @@ function crearBotonEliminar(pelicula) {
   return botonBorrar;
 }
 
-function validarCampos(pelicula) {
-  if (pelicula.titulo == "") {
-    alert("El titulo no puede ser vacio");
-  }
-}
-
-function agregarPelicula(plato) {
-   
-  //e.preventDefault();
-  const titulo = plato.Title;
-  const duracion = plato.image;
-  const director = plato.recipe;
-  const linkTrailer = plato.price;
-  console.log(titulo);
-  const pelicula = new Pelicula(titulo, duracion, director, linkTrailer);
- 
-
-  validarCampos(pelicula);
-
-  const peliculasEnLocalStorage = JSON.parse(localStorage.getItem(mesa));
-
-//   1) me traigo las cosas que necesito del locaslStorage
-//   2) Agregago mi cosa
-//   3) vuelvo a subir al localStorage.
-
-  if (peliculasEnLocalStorage == null) {
-    localStorage.setItem(mesa, JSON.stringify([pelicula]));
-    mostrarPeliculas([pelicula]);
-  } else {
-    peliculasEnLocalStorage.push(pelicula);
-    localStorage.setItem(mesa, JSON.stringify(peliculasEnLocalStorage));
-    mostrarPeliculas(peliculasEnLocalStorage);
-  }
-  //e.target.reset();
-}
-
 function eliminarPelicula(pelicula) {
+  const mesaSeleccionada = document.getElementById("platosSeleccionados");
   const peliculasEnLocalStorage = JSON.parse(localStorage.getItem(mesa));
   const nuevoArray = peliculasEnLocalStorage.filter(
     (item) => item.titulo != pelicula.titulo
   );
   localStorage.setItem(mesa, JSON.stringify(nuevoArray));
+
   mostrarPeliculas(nuevoArray);
+
+  if (nuevoArray == "") {
+    mesaSeleccionada.innerHTML = "<h1>Agregue el pedido </h1>";
+  }
 }
